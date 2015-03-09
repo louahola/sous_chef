4c0e1f51f0a17035e6f2732221aef7c461e008c4
Larry Zarou <larry@zarou.com>
2015-03-09 18:46:35 -0400

    adding proper license to headers

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,21 @@
+#
+# Cookbook Name:: sous_chef
+# Attribute::default
+#
+# Copyright 2015 Larry Zarou <larry.zarou@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 ## sous_chef
 default['sous_chef']['cookbooks'] = []
@@ -80,7 +98,7 @@ default['jenkins']['master']['install_method'] = 'package'
 default['jenkins']['master']['version'] = nil
 
 ## Gitlab Deploy Key Credentials
-default['sous_chef']['gitlab_credential_id'] = '' 
+default['sous_chef']['gitlab_credential_id'] = ''
 default['sous_chef']['gitlab_public_key'] = ''
 
 ## Gitlab Service Account Credentials
--- recipes/_cookbook_job.rb
+++ recipes/_cookbook_job.rb
@@ -1,10 +1,20 @@
 #
 # Cookbook Name:: sous_chef
-# Recipe:: _cookbook_job
+# Recipe::_cookbook_job
 #
-# Copyright (C) 2014
+# Copyright 2015 Larry Zarou <larry.zarou@gmail.com>
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 node['sous_chef']['merged_cookbooks'].each do |cookbook|
